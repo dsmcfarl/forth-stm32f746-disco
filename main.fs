3c3cccdb72ba63f25c284b2ba5376066e00850e9
Dan McFarlane <danm@mcfarlaneaviation.com>
2021-04-19 21:28:01 -0500

    add basic systick handler
    Increments a variable MS every millisecond.

--- main.fs
+++ main.fs
@@ -1,5 +1,9 @@
 #require common.fs
 #require clock.fs
+#require systick.fs
 #require debug.fs
 sys-clk-cfg
 mco1-cfg
+systick-cfg
+' systick-handler irq-systick !    \ This 'hooks' the systick-handler word (above) to the systick irq
+systick-int-enable
---
+++ systick.fs
@@ -0,0 +1,44 @@
+$E000E010 constant SYST			\ SysTick (not defined in CMSIS-SVD)
+SYST $0 + constant SYST_CSR		\ SysTick control and status register. R/W reset value = $00000000
+SYST $4 + constant SYST_RVR		\ SysTick reload value register. R/W reset value = unknown
+SYST $8 + constant SYST_CVR		\ SysTick current value register. R/W reset value = unknown  
+SYST $C + constant SYST_CALIB		\ SysTick calibration value register. Read Only, reset value = $4000493E for the STM32F7
+
+\ SYST_CSR Reset: 0x00000000
+: SYST_CSR_COUNTFLAG ( -- offset width addr ) #16 #1 SYST_CSR ;		\ (read-only) Indicates whether the counter has counted to 0 since the last read of this register
+: SYST_CSR_CLKSOURCE ( -- offset width addr ) #2 #1 SYST_CSR ;		\ (read-write) Indicates the SysTick clock source
+: SYST_CSR_TICKINT ( -- offset width addr ) #1 #1 SYST_CSR ;		\ (read-write) Indicates whether counting to 0 causes the status of the SysTick exception to change to pending
+: SYST_CSR_ENABLE ( -- offset width addr ) #0 #1 SYST_CSR ;		\ (read-write) Indicates the enabled status of the SysTick counter
+
+\ SYST_RVR Reset: unknown
+: SYST_RVR_RELOAD ( -- offset width addr ) #0 #24 SYST_RVR ;		\ (read-write) The value to load into the SYST_CVR when the counter reaches 0
+
+\ SYST_CVR Reset: unknown
+: SYST_CVR_CURRENT ( -- offset width addr ) #0 #32 SYST_CVR ;		\ (read-write) Current counter value
+
+\ For STM32F7: The SysTick calibration value is fixed to 18750, which gives a
+\ reference time base of 1 ms with the SysTick clock set to 18.75 MHz (HCLK/8,
+\ with HCLK set to 150 MHz). RM0385 Rev 8 p290 \ The RCC feeds the external clock
+\ of the Cortex System Timer (SysTick) with the AHB clock (HCLK) divided by 8.
+\ The SysTick can work either with this clock or with the Cortex clock (HCLK),
+\ configurable in the SysTick control and status register. p137
+\ SYST_CALIB Reset: $4000493E
+: SYST_CALIB_NOREF ( -- offset width addr ) #31 #1 SYST_CALIB ;		\ (read-only) Indicates whether the IMPLEMENTATIONDEFINED reference clock is implemented 
+: SYST_CALIB_SKEW ( -- offset width addr ) #30 #1 SYST_CALIB ;		\ (read-only) Indicates whether the 10ms calibration value is exact 
+: SYST_CALIB_TENMS ( -- offset width addr ) #0 #24 SYST_CALIB ;		\ (read-only) Optionally, holds a reload value to be used for 10ms (100Hz) timing 
+
+0 variable MS				\ can count to 32 bits or -> $ffffffff u. =  4294967295 mS or 4294967 seconds, or 71582 minutes or 1193 hours.
+
+: systick-int-enable ( -- )
+  SYST_CSR_TICKINT bfs!
+;
+: systick-handler ( -- )
+  MS @
+  1+
+  MS !
+;
+: systick-cfg ( -- )
+  SYST_CSR_CLKSOURCE bfs!		\ use system clock
+  #216000 SYST_RVR_RELOAD bf!
+  SYST_CSR_ENABLE bfs!
+;
