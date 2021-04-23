df25058d38b1ae792b668a13b161129380dd0726
Dan McFarlane <danm@mcfarlaneaviation.com>
2021-04-23 16:17:11 -0500

    add gpio constants

--- gpio.fs
+++ gpio.fs
@@ -5,6 +5,27 @@
 %10 constant AF
 %11 constant ANALOG
 
+#0 constant AF0
+#1 constant AF1
+#2 constant AF2
+#3 constant AF3
+#4 constant AF4
+#5 constant AF5
+#6 constant AF6
+#7 constant AF7
+#8 constant AF8
+#9 constant AF9
+#10 constant AF10
+#11 constant AF11
+#12 constant AF12
+#13 constant AF13
+#14 constant AF14
+#15 constant AF15
+%00 constant LOW_SPD
+%01 constant MED_SPD
+%10 constant HIGH_SPD
+%11 constant VERY_HIGH_SPD
+
 : enable-gpiok-clock ( -- ) RCC_AHB1ENR_GPIOKEN bfs! ;
 : enable-gpioa-clock ( -- ) RCC_AHB1ENR_GPIOAEN bfs! ;
 : gpiok3-mode! ( %bb -- ) GPIOK_MODER_MODER3 bf! ;
