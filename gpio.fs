8f8ffb98b1e930785608eeebacfdeb4f737a2146
Dan McFarlane <danm@mcfarlaneaviation.com>
2021-04-22 19:20:09 -0500

    re-format gpio.fs

--- gpio.fs
+++ gpio.fs
@@ -5,13 +5,7 @@
 %10 constant AF
 %11 constant ANALOG
 
-: enable-gpiok-clock ( -- )
-  RCC_AHB1ENR_GPIOKEN bfs!
-;
-
-: enable-gpioa-clock ( -- )
-  RCC_AHB1ENR_GPIOAEN bfs!
-;
-: set-gpiok3-output-mode
-  %01 GPIOK_MODER_MODER3 bf!
-;
+: enable-gpiok-clock ( -- ) RCC_AHB1ENR_GPIOKEN bfs! ;
+: enable-gpioa-clock ( -- ) RCC_AHB1ENR_GPIOAEN bfs! ;
+: gpiok3-mode! ( %bb -- ) GPIOK_MODER_MODER3 bf! ;
+: gpioa8-mode! ( %bb -- ) GPIOA_MODER_MODER8 bf! ;
