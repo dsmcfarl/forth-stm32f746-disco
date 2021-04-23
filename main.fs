5de492c154a5a199d4d0232f8a1e8b6181047cfd
Dan McFarlane <danm@mcfarlaneaviation.com>
2021-04-22 20:28:06 -0500

    add initial lcd code from mecrisp st32f746-ra/demo.fs

--- main.fs
+++ main.fs
@@ -1,5 +1,6 @@
 #require common.fs
 #require clock.fs
+
 #require systick.fs
 #require debug.fs
 cfg-sysclk
@@ -7,3 +8,5 @@ mco1-cfg
 systick-cfg
 ' systick-handler irq-systick !    \ This 'hooks' the systick-handler word (above) to the systick irq
 systick-interrupt-enable
+#require lcd.fs
+test-genchar
