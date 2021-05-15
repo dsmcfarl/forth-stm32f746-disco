838f75bf5ed8d26cdc00f50d5decbebf94eee0ab
Dan McFarlane <danm@mcfarlaneaviation.com>
2021-05-15 16:15:50 -0500

    refactor i2c.fs

--- i2c.fs
+++ i2c.fs
@@ -1,75 +1,66 @@
+\ I2C1 driver for master send and receive, 7 bit addressing, 400KHz.
+
+
 \ Wait while the word test with address 'test results in a true, or until
-\ timeout ends.  Returns status = 0 if exits before timeout, returns
-\ status = 1 if timeout occurs. 
-: timed ( 'test -- status )
-    $FFFFF                             \ Timeout value
-    begin swap dup execute while
-      swap 1- dup 0= if
-        2drop 1 exit                  \ Timed out
-      then
-    repeat
-    2drop 0                           \ 'test became false before timeout 
+\ timeout ends.  Returns false if 'test execute returns false before timeout,
+\ otherwise true.
+$ffff constant TIMEOUT
+: true-until-timeout? ( 'test -- flag )
+  TIMEOUT
+  begin swap dup execute while
+    swap 1- dup 0= if
+      2drop true exit			\ timed out
+    then
+  repeat
+  2drop false				\ 'test became false before timeout 
 ; 
 
-: af4-med-open-drain! ( port pin -- )
+: init-i2c-gpio-pin  ( port pin -- )
   2dup OPEN_DRAIN -rot otyper!
   2dup AF         -rot moder!
   2dup MED        -rot ospeedr!
        AF4        -rot afr!
 ;
 
-\ -------------------------------------------------------------------------
-\  I2C1 driver for master send and receive, 7 bit addressing.
-\  I2C speed is determined by value of variable i2c1-Fast (0 = 100 KHz,
-\  1 = 400 KHz).  Timing values are from Table 76 of RM0360 for I2CCLK =
-\  SYSCLK = 48 MHz.
-\ -------------------------------------------------------------------------
-
 %10 constant I2C1_HSI
-: i2c1-init ( -- )
-  GPIOB 8 af4-med-open-drain!
-  GPIOB 9 af4-med-open-drain!
-  RCC APB1ENR_I2C1EN bfs!		\ Enable peripheral clock
-  I2C1_HSI RCC DCKCFGR2_I2C1SEL bf!	\ I2C1 clock source is HSI
-  RCC APB1RSTR_I2C1RST bfs!		\ reset I2C1 
-  RCC APB1RSTR_I2C1RST bfc!
-  I2C1 CR1_PE bfc!			\ Disable I2C1
+: enable-i2c1-clock ( -- ) RCC APB1ENR_I2C1EN bfs! ;
+: set-i2c1-clock-hsi ( -- ) I2C1_HSI RCC DCKCFGR2_I2C1SEL bf! ;
+: reset-i2c1 ( -- ) RCC APB1RSTR_I2C1RST bfs! RCC APB1RSTR_I2C1RST bfc! ;
+: disable-i2c1 ( -- ) I2C1 CR1_PE bfc! ;
+: config-i2c1-timing ( -- )
   \ Ref: RM0385 p 990 (table 182) for example timing
-  \ TODO: define constants for these values
-  $9 TIMINGR_SCLL bf<<
-  $3 TIMINGR_SCLH bf<< or
-  $1 TIMINGR_SDADEL bf<< or
-  $3 TIMINGR_SCLDEL bf<< or
-  $0 TIMINGR_PRESC bf<< or I2C1_TIMINGR !
-  I2C1 CR1_PE bfs!			\ Enable I2C1
+  $9 TIMINGR_SCLL bf<<				\ SCL low period: tSCLL = (SCLL+1) x tPRESC
+  $3 TIMINGR_SCLH bf<< or			\ SCL high period: tSCLH = (SCLH+1) x tPRESC
+  $1 TIMINGR_SDADEL bf<< or			\ SDA delay: tSDADEL= SDADEL x tPRESC
+  $3 TIMINGR_SCLDEL bf<< or			\ SCL delay: tSCLDEL = (SCLDEL+1) x tPRESC
+  $0 TIMINGR_PRESC bf<< or I2C1_TIMINGR !	\ tPRESC = (PRESC+1) x tI2CCLK
+;
+: enable-i2c1 ( -- ) I2C1 CR1_PE bfs! ;
+: i2c1-init ( -- )
+  GPIOB 8 init-i2c-gpio-pin
+  GPIOB 9 init-i2c-gpio-pin
+  enable-i2c1-clock
+  set-i2c1-clock-hsi
+  reset-i2c1
+  disable-i2c1
+  config-i2c1-timing
+  enable-i2c1
 ;
 
-\ These words are used for timed waits
-\ Return true if I2C1 busy, false if not busy
 : i2c1-busy? ( -- flag ) I2C1 ISR_BUSY bf@ ;
+: i2c1-rxdr-empty? ( -- flag ) I2C1 ISR_RXNE bf@ 0= ;
+: i2c1-txdr-not-empty? ( -- flag ) I2C1 ISR_TXIS bf@ 0= ;
+: i2c1-stop-flag-not-set? ( -- flag) I2C1 ISR_STOPF bf@ 0= ;
 
-\ Return true if the I2C1 rx buffer is empty
-: i2c1-nrxne-new? ( -- flag ) I2C1 ISR_RXNE bf@ 0= ;
-: i2c1-nrxne? ( -- flag )  %1 2 lshift I2C1_ISR bit@ not ;
-
-: i2c1-ntxis? ( -- flag ) I2C1 ISR_TXIS bf@ 0= ;
-
-\ Return true if stop flag not set
-: i2c1-nstopf? ( -- flag) I2C1 ISR_STOPF bf@ 0= ;
-
-\ I2C1 read/write nbytes bytes into/from buffer with address buf-addr
-\ to slave with (left-shifted) address slave-addr
-: i2c1-read ( buf-addr slave-addr nbytes -- exit-status )
-
-  \ If nbytes = 0, nothing to do
-  dup 0= if
-    2drop drop 0 exit
-  then
-  
-  ['] i2c1-busy? timed if
+: log.debug ( c-addr length -- )
+  cr cr ." DEBUG: " type
+;
+: timed-out-waiting-for-i2c1? ( -- flag ) ['] i2c1-busy? true-until-timeout? ;
+: i2c1-read ( buf-addr slave-addr nbytes -- flag )
+  timed-out-waiting-for-i2c1? if
     2drop drop 
-    cr cr ." DEBUG: i2c1-read: bus is busy timeout" cr
-    1 exit
+    s" i2c1-read: bus is busy timeout" log.debug
+    true exit
   then
   \ Prepare for transfer
   $0 I2C1_CR2 !                 \ Clear register
@@ -80,7 +71,7 @@
 
   0 do  \ nbyte consumed, only buf-addr remains
     \ Wait until rx buffer has data
-    ['] i2c1-nrxne? timed if
+    ['] i2c1-rxdr-empty? true-until-timeout? if
       drop 
       cr cr ." DEBUG: i2c1-read: rx buffer empty timeout" cr
       2 unloop exit
@@ -93,14 +84,14 @@
   0   \ No error exit status
 ;
 
-: i2c1-write ( buf-addr slave-addr nbytes -- exit-status )
+: i2c1-write ( buf-addr slave-addr nbytes -- status )
 
   \ If nbytes = 0, nothing to do
   dup 0= if 
     2drop drop 0 exit
   then
   
-  ['] i2c1-busy? timed if
+  ['] i2c1-busy? true-until-timeout? if
     2drop drop 
     cr cr ." DEBUG: i2c1-write: bus is busy timeout" cr
     1 exit
@@ -117,7 +108,7 @@
   I2C1 ISR_TXIS bfs!
   0 do  \ nbyte consumed, only buf-addr remains
     \ Wait until tx buffer is empty
-    ['] i2c1-ntxis? timed if
+    ['] i2c1-txdr-not-empty? true-until-timeout? if
       drop 
       cr cr ." DEBUG: i2c1-write: tx buffer not empty timeout" cr
       2 unloop exit
@@ -132,7 +123,7 @@
   loop
   drop    \ Drop buf-addr
 
-  ['] i2c1-nstopf? timed if
+  ['] i2c1-stop-flag-not-set? true-until-timeout? if
     cr cr ." DEBUG: i2c1-write: no stop generated timeout" cr
     3 exit
   then
