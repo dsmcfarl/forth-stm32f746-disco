cc0e92fcacf7301c5d3b986e2a76d06189bb0bd6
Dan McFarlane <danm@mcfarlaneaviation.com>
2021-05-16 16:55:10 -0500

    add features to log.fs

--- log.fs
+++ log.fs
@@ -1,2 +1,7 @@
-: log.debug ( c-addr length -- ) cr cr ." DEBUG: " type ;
-: log.warning ( c-addr length -- ) cr cr ." WARNING: " type ;
+\ TODO: allow setting a log level and use to decide what to show
+: log.debug ( c-addr length -- ) cr ." DEBUG: " type ;
+: log.debug-append ( c-addr length -- ) type ;
+: log.info ( c-addr length -- ) cr ." INFO " type ;
+: log.info-append ( c-addr length -- ) type ;
+: log.warning ( c-addr length -- ) cr ." WARNING: " type ;
+: log.warning-append ( c-addr length -- ) type ;
